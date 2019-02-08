b21ce599210245a5706c919958749aff4c129b36
finisher1017 <finisher1017@gmail.com>
2019-02-08 17:48:12 +0000

    Completed Section 31

--- RESTful/RESTfulBlogApp/app.js
+++ RESTful/RESTfulBlogApp/app.js
@@ -3,11 +3,13 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 
@@ -21,12 +23,6 @@ var blogSchema = new mongoose.Schema({
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
-
-// Blog.create({
-//   title: "Another Test Blog",
-//   image: "https://images.unsplash.com/photo-1549383761-180102efa02c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-//   body: "ANOTHER TEST BLOG POST"
-// });
 
 
 // RESTFUL ROUTES
@@ -45,6 +41,7 @@ app.get("/blogs", function(req, res) {
     });
 });
 
+
 //CREATE BLOGS GET ROUTE
 app.get("/blogs/new", function(req, res) {
     res.render("new");
@@ -53,6 +50,7 @@ app.get("/blogs/new", function(req, res) {
 // CREATE BLOGS POST ROUTE
 app.post("/blogs", function(req, res) {
     //create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog) {
         if(err){
             res.render("new");
@@ -89,6 +87,10 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res) {
+    console.log(req.body.blog);
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    console.log("=======================");
+    console.log(req.body.blog);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
         if(err) {
             res.redirect("/blog");
@@ -101,8 +103,16 @@ app.put("/blogs/:id", function(req, res) {
 
 // DELETE ROUTE
 app.delete("/blogs/:id", function(req, res) {
-    res.send("DESTROY ROUTE");
-})
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if(err) {
+            res.redirect("/blogs");
+            console.log(err);
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
