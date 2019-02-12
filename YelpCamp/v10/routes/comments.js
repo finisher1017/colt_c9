23a1eeb49e1e0376dfee15df8753b2e7abe54388
finisher1017 <finisher1017@gmail.com>
2019-02-12 18:53:51 +0000

    complete delete comment

--- YelpCamp/v10/routes/comments.js
+++ YelpCamp/v10/routes/comments.js
@@ -72,6 +72,18 @@ router.put("/:comment_id", function(req, res) {
 });
 
 
+// DESTROY COMMENT
+router.delete("/:comment_id", function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if(err){
+            res.redirect("back");
+        } else {
+            res.redirect(`/campgrounds/${req.params.id}`);
+        }
+    });
+});
+
+
 //MIDDLEWARE
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
